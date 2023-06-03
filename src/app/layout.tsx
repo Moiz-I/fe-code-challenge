5d04b97f3b5eac60838446546790126913d3709b
Moiz-I <sky.moizi9@gmail.com>
2023-06-03 21:09:27 +0100

    fix hydration error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import ClientOnly from "./ClientOnly";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -15,7 +16,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ClientOnly>{children}</ClientOnly>
+      </body>
     </html>
   );
 }
